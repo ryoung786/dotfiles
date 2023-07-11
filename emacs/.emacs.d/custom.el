0e28cfb9f8b45049e768de045efb26a7df79f8eb
Ryan Young <ryoung786@gmail.com>
2023-07-10 22:53:42 -0600

    corfu-auto on

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -5,18 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(lsp-credo-version "0.2.0")
  '(package-selected-packages
-   '(nerd-icons-dired all-the-icons-dired embark-consult consult
-		      kind-icon orderless corfu corfu-prescient embark
-		      marginalia vertico vertico-posframe
-		      vertico-prescient ripgrep hungry-delete
-		      vterm-toggle multi-vterm vterm projectile
-		      default-text-scale doom-modeline doom-themes
-		      elixir-ts-mode elixir-mode yaml-mode
-		      rainbow-delimiters gh-md forge flx-ido company
-		      web-mode inf-elixir mix flycheck lsp-treemacs
-		      lsp-ui lsp-mode exunit load-env-vars which-key
-		      smartparens yasnippet-snippets yasnippet magit
-		      exec-path-from-shell treesit-auto)))
+   '(nerd-icons-completion nerd-icons-dired all-the-icons-dired embark-consult consult kind-icon orderless corfu corfu-prescient embark marginalia vertico vertico-posframe vertico-prescient ripgrep hungry-delete vterm-toggle multi-vterm vterm projectile default-text-scale doom-modeline doom-themes elixir-ts-mode elixir-mode yaml-mode rainbow-delimiters gh-md forge flx-ido company web-mode inf-elixir mix flycheck lsp-treemacs lsp-ui lsp-mode exunit load-env-vars which-key smartparens yasnippet-snippets yasnippet magit exec-path-from-shell treesit-auto)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
