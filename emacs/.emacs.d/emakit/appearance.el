1e14e5f9458307223646821a442d05245b555422
Ryan Young <ryoung786@gmail.com>
2023-08-31 22:52:23 -0600

    My theme changes only affect doom-material

--- emacs/.emacs.d/emakit/appearance.el
+++ emacs/.emacs.d/emakit/appearance.el
@@ -6,34 +6,6 @@
 ;;
 ;; http://github.com/walter/emakit
 
-(use-package emacs
-  :custom-face
-  (ansi-color-black ((t (:background "dark orchid" :foreground "dark orchid"))))
-  (ansi-color-bright-black ((t (:background "MediumPurple1" :foreground "MediumPurple1"))))
-  (ansi-color-bright-magenta ((t (:background "orchid1" :foreground "orchid1"))))
-  (ansi-color-magenta ((t (:background "magenta" :foreground "magenta"))))
-  (blamer-face ((t :foreground "#7a88cf" :background nil :height 140 :italic t)))
-  (fixed-pitch ((t (:family "Inconsolata"))))
-  (font-lock-comment-face ((t (:foreground "bisque3" :slant italic :family "Fira Code iScript"))))
-  (font-lock-doc-face ((t (:inherit font-lock-comment-face :foreground "bisque3"))))
-  (flymake-error-echo ((t :foreground "LightCoral" :weight normal)))
-  (header-line ((t (:inherit nil :background "DarkOrchid4"))))
-  (hl-line ((t (:extend t :background "DarkSlateGray"))))
-  (isearch ((t (:background "hotpink" :foreground "white"))))
-  (lazy-highlight ((t (:inherit match :background "LightCyan4"))))
-  (line-number ((t (:inherit default :foreground "gray60" :slant italic :weight normal))))
-  (line-number-current-line ((t (:inherit (hl-line default) :background "DarkSlateGray" :foreground "snow1"))))
-  (magit-hash ((t (:foreground "gray65"))))
-  (markdown-blockquote-face ((t (:foreground "dark gray"))))
-  (mode-line ((t (:background "DarkSlateGray" :foreground "snow1" :box (:line-width (1 . 1) :color "snow" :style flat-button)))))
-  (mode-line-inactive ((t (:background "gray30" :foreground "#f2fffc" :box (:line-width (1 . 1) :color "gray30" :style released-button)))))
-  (region ((t (:background "DarkSlateGray"))))
-  (shadow ((t (:foreground "gray50"))))
-  (vertico-current ((t (:background "DarkSlateGray"))))
-  (vterm-color-black ((t (:background "MediumPurple1" :foreground "#19181A"))))
-  (web-mode-variable-name-face ((t (:inherit font-lock-variable-name-face :foreground "plum"))))
-  )
-
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -44,9 +16,6 @@
 (use-package eldoc :delight eldoc-mode)
 
 (setq help-window-select t)
-
-(use-package doom-themes
-  :config (load-theme 'doom-material t))
 
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
@@ -68,10 +37,43 @@
   :custom
   (highlight-indent-guides-auto-enabled nil)
   (highlight-indent-guides-method 'character)
-  (highlight-indent-guides-responsive 'top)
+  ;; (highlight-indent-guides-responsive 'top)
   :custom-face
   (highlight-indent-guides-character-face ((t (:foreground "gray40")))))
 
-(setq flymake-mode-line-lighter "") ; Don't display "Flymake[0 0]", instead just "[0 0]"
+; Don't display "Flymake[0 0]", instead just "[0 0]"
+(setq flymake-mode-line-lighter "")
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-material t)
+  (custom-theme-set-faces
+   'doom-material
+   '(ansi-color-black ((t (:background "dark orchid" :foreground "dark orchid"))))
+   '(ansi-color-bright-black ((t (:background "MediumPurple1" :foreground "MediumPurple1"))))
+   '(ansi-color-bright-magenta ((t (:background "orchid1" :foreground "orchid1"))))
+   '(ansi-color-magenta ((t (:background "magenta" :foreground "magenta"))))
+   '(blamer-face ((t :foreground "#7a88cf" :background nil :height 140 :italic t)))
+   '(fixed-pitch ((t (:family "Inconsolata"))))
+   '(font-lock-comment-face ((t (:foreground "bisque3" :slant italic :family "Fira Code iScript"))))
+   '(font-lock-doc-face ((t (:inherit font-lock-comment-face :foreground "bisque3"))))
+   '(flymake-error-echo ((t :foreground "LightCoral" :weight normal)))
+   '(header-line ((t (:inherit nil :background "DarkOrchid4"))))
+   '(hl-line ((t (:extend t :background "DarkSlateGray"))))
+   '(isearch ((t (:background "hotpink" :foreground "white"))))
+   '(lazy-highlight ((t (:inherit match :background "LightCyan4"))))
+   '(line-number ((t (:inherit default :foreground "gray60" :slant italic :weight normal))))
+   '(line-number-current-line ((t (:inherit (hl-line default) :background "DarkSlateGray" :foreground "snow1"))))
+   '(magit-hash ((t (:foreground "gray65"))))
+   '(markdown-blockquote-face ((t (:foreground "dark gray"))))
+   '(mode-line ((t (:background "DarkSlateGray" :foreground "snow1" :box (:line-width (1 . 1) :color "snow" :style flat-button)))))
+   '(mode-line-inactive ((t (:background "gray30" :foreground "#f2fffc" :box (:line-width (1 . 1) :color "gray30" :style released-button)))))
+   '(region ((t (:background "DarkSlateGray"))))
+   '(shadow ((t (:foreground "gray50"))))
+   '(vertico-current ((t (:background "DarkSlateGray"))))
+   '(vterm-color-black ((t (:background "MediumPurple1" :foreground "#19181A"))))
+   '(web-mode-variable-name-face ((t (:inherit font-lock-variable-name-face :foreground "plum")))))
+  (enable-theme 'doom-material)
+  )
 
 (provide 'appearance)
