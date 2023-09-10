b7e81f09b7d4ce09521fc93b897d9b3285e61e3d
Ryan Young <ryoung786@gmail.com>
2023-09-09 22:47:23 -0400

    Jinx spellchecker, which depends on enchant library

--- emacs/.emacs.d/emakit/font.el
+++ emacs/.emacs.d/emakit/font.el
@@ -4,4 +4,9 @@
 (use-package default-text-scale
   :init (default-text-scale-mode 1))
 
+;; Improved Spell Checker (depends on enchant system library)
+(use-package jinx
+  :hook (emacs-startup . global-jinx-mode)
+  :bind (("C-M-$" . jinx-correct)))
+
 (provide 'font)
