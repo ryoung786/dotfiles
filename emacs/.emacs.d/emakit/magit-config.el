6e16a3dc463bc95b997736aec98d61188423b79b
Ryan Young <ryoung786@gmail.com>
2023-09-09 20:35:10 -0400

    Magit git gutter lines instead of characters

--- emacs/.emacs.d/emakit/magit-config.el
+++ emacs/.emacs.d/emakit/magit-config.el
@@ -13,9 +13,12 @@
   (global-set-key (kbd "C-x g") 'magit-status)
   :bind ("s-i" . magit-blame))
 
-(use-package git-gutter-fringe
+(use-package git-gutter
   :hook (prog-mode . git-gutter-mode)
   :bind ("C-c s" . git-gutter:stage-hunk)
+  :config (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
   :config
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
