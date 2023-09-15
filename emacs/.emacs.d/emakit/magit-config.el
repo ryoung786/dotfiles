0a4de478379712692b8f5ca9927eff164fc4a57a
Ryan Young <ryoung786@gmail.com>
2023-09-15 11:04:18 -0400

    browse-at-remote for opening file/line in github

--- emacs/.emacs.d/emakit/magit-config.el
+++ emacs/.emacs.d/emakit/magit-config.el
@@ -25,4 +25,7 @@
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
 
+(use-package browse-at-remote
+  :bind ("C-c g g" . 'browse-at-remote))
+
 (provide 'magit-config)
