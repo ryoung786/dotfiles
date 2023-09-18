f0ce5cf9aab246f9ffaea4fe94cb57fb9458339a
Ryan Young <ryoung786@gmail.com>
2023-09-18 14:55:56 -0400

    Add magit to project prefix map and switch commands

--- emacs/.emacs.d/emakit/projectile-config.el
+++ emacs/.emacs.d/emakit/projectile-config.el
@@ -5,7 +5,10 @@
   :init (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package project
-  :custom (project-prompter 'project-prompt-project-name))
+  :custom (project-prompter 'project-prompt-project-name)
+  :config (add-to-list 'project-switch-commands '(magit-project-status "Magit" 109) 'append)
+  :bind (:map project-prefix-map ("m" . magit-project-status))
+  )
 
 (use-package ripgrep)
 
