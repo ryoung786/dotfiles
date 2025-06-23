f30a55bf73be45a4e513276a50abef5cb6c5779b
Ryan Young <ryoung786@gmail.com>
2025-06-23 12:59:00 -0400

    delete old emakit configs; move lexical binary; mise config no latest

--- xdg/emacs/emakit/imenu-config.el
+++
@@ -1,5 +0,0 @@
-(use-package imenu-list
-  :bind ("<f2>" . imenu-list-smart-toggle)
-  :custom (imenu-list-focus-after-activation t))
-
-(provide 'imenu-config)
--- xdg/emacs/emakit/sql-config.el
+++
@@ -1,7 +0,0 @@
-(use-package emacs
-  :custom
-  (sql-postgres-login-params '(user database server port))
-  :init
-  (add-hook 'sql-mode-hook (lambda () (setq tab-width 2))))
-
-(provide 'sql-config)
